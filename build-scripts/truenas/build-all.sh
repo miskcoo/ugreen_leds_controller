02edae467ac6e754e0cc23089ddfc3b06694aa53
Yuhao Zhou <miskcoo@gmail.com>
2024-08-22 20:12:38 +0800

    auto push

--- build-scripts/truenas/build-all.sh
+++ build-scripts/truenas/build-all.sh
@@ -2,6 +2,9 @@
 truenas_versions=$(curl https://download.truenas.com/ | grep -Po "(?<=href=\")TrueNAS-SCALE-\w*?/\d+(\.\d+)*" | uniq)
 
 for version in ${truenas_versions}; do 
-    echo Building "$version"
-    bash build.sh "$version"
+    # only build for TrueNAS-SCALE-Dragonfish/24.04.0 and later versions
+    if [ $(echo $version | grep -Po "(?<=/)\d+") -ge 24 ] && [ ! -d "build/$version" ]; then
+        echo Building "$version"
+        bash build.sh "$version"
+    fi
 done
